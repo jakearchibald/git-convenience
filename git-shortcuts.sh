5929c2970287dfe95434339306f4c16541713400
Jake Archibald <jaffathecake@gmail.com>
2012-09-01 16:09:27 +0100

    Favouring git-up in aliases

--- git-shortcuts.sh
+++ git-shortcuts.sh
@@ -1,9 +1,16 @@
 alias gs='git status'
 alias gaa='git add -A'
-alias gp='git pull && git push'
 alias gdiff='git diff --color-words'
 alias gclean='git gc --prune=now && git remote prune origin'
 alias glog='git log --graph --oneline --all --decorate'
+
+if type git-up -t > /dev/null 2>&1; then
+	alias gup='git-up'
+	alias gp='git-up && git push'
+else
+	alias gup='git pull'
+	alias gp='git pull && git push'	
+fi
 
 function gc {
 	git commit -am "$1"

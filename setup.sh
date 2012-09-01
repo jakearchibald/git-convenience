bac68777e3a11a494c49878bd8e00878e372f423
Jake Archibald <jaffathecake@gmail.com>
2012-09-01 16:10:01 +0100

    Proper download & trial script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,42 @@
-# If no git, fail & exit
-# Add param that doesn't do the saving stuff
+command -v git >/dev/null 2>&1 || { 
+	echo >&2 "git not found - install git & try again"
+	exit 1
+}
 
-# Decide on .bashrc or .bash_profile
-# Download the other 3
+function git_load_helper {
+	if [ "$4" = "trial" ]; then
+		echo "Trialing $1"
+		eval "$(curl -Ls $1)"
+	else
+		echo "Saving $1"
+		curl -Lso "$2" "$1"
+		source "$2"
+		echo "source \"$2\"  # Added by git-convenience" >> "$3"
+	fi
+}
 
-# Only if git completion not already here...
-curl -#Lo ~/.git-completion.bash http://jakemac/git-tools/git-completion.bash
-# add source... to rc/profile
-# Run for the current session
+function setup_git_convenience {
+	# Decide on .bashrc or .bash_profile
+	local bash_startup_file="$HOME/.bashrc"
+	if [[ $(uname) = Darwin* ]]; then
+		local bash_startup_file="$HOME/.bash_profile"
+	fi
 
-curl -#Lo ~/.terminal-prefs.sh http://jakemac/git-tools/terminal-prefs.sh
-curl -#Lo ~/.git-shortcuts.sh http://jakemac/git-tools/git-shortcuts.sh
-# add source... to rc/profile
-# Run for the current session
+	# If git completion not installed...
+	if ! type -t __gitcomp > /dev/null 2>&1; then
+		git_load_helper https://raw.github.com/git/git/e90020cdb3273af3b0c7915c0aacf16b19bbf994/contrib/completion/git-completion.bash "$HOME/.git-completion.bash" "$bash_startup_file" "$1"
+	fi
+
+	# If git prompt not installed...
+	if ! type -t __git_ps1 > /dev/null 2>&1; then
+		git_load_helper https://raw.github.com/git/git/e90020cdb3273af3b0c7915c0aacf16b19bbf994/contrib/completion/git-prompt.sh "$HOME/.git-prompt.sh" "$bash_startup_file" "$1"
+	fi
+
+	git_load_helper http://jakemac.local/git-tools/terminal-prefs.sh "$HOME/.terminal-prefs.sh" "$bash_startup_file" "$1"
+	git_load_helper http://jakemac.local/git-tools/git-shortcuts.sh "$HOME/.git-shortcuts.sh" "$bash_startup_file" "$1"
+
+	unset setup_git_convenience
+	unset git_load_helper
+}
+
+setup_git_convenience
